be30f454502978d8eb3305404d40dc08449b31ae
Botmasher <languagejosh@hotmail.com>
2018-05-09 08:28:21 -1000

    Add controlled input component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      sentence: ''
+    };
+  }
+
+  handleInput = e => this.setState({ sentence: e.target.value });
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" value={this.state.sentence} onChange={this.handleInput} />
       </div>
     );
   }
